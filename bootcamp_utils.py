3a2471415485182bf1a9aca4b2ec16a85701d3e7
Ethan Simonoff <esimonof@caltech.edu>
2017-07-18 13:23:33 -0700

    Fixed wrong import of numpy

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -1,3 +1,11 @@
 """Contains utilities and code for use in the 2017 Python bootcamp."""
 
-import numpy
+import numpy as np
+
+def ecdf(data):
+    """Compute x, y values for an empirical distribution function."""
+
+    x = np.sort(data)
+    y = np.arange(1, len(data)+1) / len(data)
+
+    return x, y
